4e07f9173dd37bbf0f077a22227c9a2bf968f81b
krishnoit <krishnoit100@gmail.com>
2023-05-20 07:32:11 +0530

    FEAT : main -> master in main.yml

--- dist/src/router_controller/signup/controller/singnup_post_controller.js
+++ dist/src/router_controller/signup/controller/singnup_post_controller.js
@@ -1,34 +1,34 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.signupPost = void 0;
 const bcrypt_1 = __importDefault(require("bcrypt"));
 const helper_1 = __importDefault(require("../../../config/helper"));
 const signupPost = async (req, res) => {
-    try {
-        bcrypt_1.default.hash(req.body.password, 10, async (error, hash) => {
-            if (error) {
-                res.status(500).send(error);
-            }
-            else {
-                const info = await helper_1.default.user.create({
-                    data: Object.assign(Object.assign({}, req.body), { password: hash }),
-                });
-                res.status(200).json({
-                    message: "User created successfully",
-                    data: info,
-                });
-            }
+  try {
+    bcrypt_1.default.hash(req.body.password, 10, async (error, hash) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        const info = await helper_1.default.user.create({
+          data: Object.assign(Object.assign({}, req.body), { password: hash }),
         });
-    }
-    catch (error) {
-        res.status(500).send(error.message);
-    }
+        res.status(200).json({
+          message: "User created successfully",
+          data: info,
+        });
+      }
+    });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 exports.signupPost = signupPost;
 module.exports = {
-    signupPost: exports.signupPost,
+  signupPost: exports.signupPost,
 };
 //# sourceMappingURL=singnup_post_controller.js.map
