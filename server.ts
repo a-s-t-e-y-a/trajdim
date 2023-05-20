cd77f360a3afdded584c0200c3bbda16508799d3
krishnoit <krishnoit100@gmail.com>
2023-05-20 07:20:07 +0530

    FEAT : port no change

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import http from "http";
 import app from "./src/app";
 
 const server = http.createServer(app);
-const PORT = 4000;
+const PORT = 5000;
 server.listen(PORT, () => {
   console.log("You are now connected");
 });
