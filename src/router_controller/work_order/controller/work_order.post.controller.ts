c242e7f6c773e4924c01c498b34d785b964f9be2
krishnoit <krishnoit100@gmail.com>
2023-05-26 11:45:55 +0530

    FEAT : work route route set

--- src/router_controller/work_order/controller/work_order.post.controller.ts
+++ src/router_controller/work_order/controller/work_order.post.controller.ts
@@ -8,11 +8,15 @@ export const work_orderPOST = async (
   res: Response
 ): Promise<any> => {
   try {
+    const {item,...rest} = req.body
     const data1 = await prisma.work_order.create({
       data: {
-        ...req.body,
         user: req.user.id,
-      },
+        item:{
+          create:item || []
+        },
+        ...rest
+      }
     });
     res.status(200).json({
       message: "work order created successfuly",
