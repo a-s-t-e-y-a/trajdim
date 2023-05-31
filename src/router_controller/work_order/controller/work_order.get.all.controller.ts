eb87502797c49e47c12d04d9f95a0345c5c4861a
krishnoit <krishnoit100@gmail.com>
2023-05-31 12:35:07 +0530

    FEAT : route added in work order

---
+++ src/router_controller/work_order/controller/work_order.get.all.controller.ts
@@ -0,0 +1,29 @@
+import prisma from "../../../config/helper";
+import { Request, Response } from "express";
+
+interface AuthenticatedRequest extends Request {
+  user?: any;
+}
+
+export const work_orderGETAll = async (
+  req: AuthenticatedRequest,
+  res: Response
+): Promise<any> => {
+  try {
+    const { id } = req.params;
+    const workOrder = await prisma.work_order.findMany();
+
+    if (!workOrder) {
+      return res.status(404).json({
+        message: "work order not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "work order retrieved successfully",
+      data: workOrder,
+    });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
--- src/router_controller/work_order/work_order.route.ts
+++ src/router_controller/work_order/work_order.route.ts
@@ -4,6 +4,7 @@ import { work_orderPOST } from "./controller/work_order.post.controller";
 import { work_orderPUT } from "./controller/work_order.edit.controller";
 import { work_orderDELETE } from "./controller/work_order.delete.controller";
 import { work_orderGET } from "./controller/work_order.get.controller";
+import { work_orderGETAll } from "./controller/work_order.get.all.controller";
 
 const work_order = express.Router();
 
@@ -11,4 +12,5 @@ work_order.post("/work_order", authenticateToken, work_orderPOST);
 work_order.put("/work_order/:id", authenticateToken, work_orderPUT);
 work_order.delete("/work_order/:id", authenticateToken, work_orderDELETE);
 work_order.get("/work_order/:id", authenticateToken, work_orderGET);
+work_order.get("/work_order", authenticateToken, work_orderGETAll);
 export default work_order;
