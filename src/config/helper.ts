e6459c5e6c5e72a32b2a8166dbe73e7960b0946f
a-s-t-e-y-a <krishnoit100@gmail.com>
2024-03-26 20:23:39 +0530

    new

--- src/config/helper.ts
+++ src/config/helper.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: "mongodb+srv://krishnoit:06zau35XfEkIANf6@cluster0.1asve3f.mongodb.net/trajdim?retryWrites=true&w=majority&connectTimeoutMS=5000", // Adjust the connection URL with the desired lock timeout
+      url: "mongodb+srv://krishnoit:58ThD1r5Kq4OIZ2y@cluster0.w7eey7o.mongodb.net/trajdim?retryWrites=true&w=majority&appName=Cluster0", // Adjust the connection URL with the desired lock timeout
     },
   },
 });
