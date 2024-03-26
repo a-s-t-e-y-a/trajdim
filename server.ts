0613fa9dd2c8133c7c40ae0aa4d12af4c24bf316
a-s-t-e-y-a <krishnoit100@gmail.com>
2024-03-26 20:27:10 +0530

    new

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import http from "http";
 import app from "./src/app";
 
 const server = http.createServer(app);
-const PORT = 5000;
+const PORT = 5300;
 server.listen(PORT, () => {
   console.log("You are now connected");
 });
